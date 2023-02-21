2d81ab5f45eb5f41cddc475c328f5bb80c1dc984
Bilgehan Kalay <kalaybilgehan60@gmail.com>
2023-02-21 23:08:38 +0300

    .

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,12 +11,13 @@ function Home() {
   return (
     <div>
       <div>
-        <Canvas>
+        {/* <Canvas>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
           <Box position={[-1.2, 0, 0]} />
           <Box position={[1.2, 0, 0]} />
-        </Canvas>
+        </Canvas> */}
+        <h1>Home</h1>
       </div>
     </div>
   );
